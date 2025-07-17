46435e28c99d60ee52b7b918b3bd608595919e36
Christian Montoya Carrasco <christianmontoyacarrasco@Christians-MacBook-Pro.local>
2025-07-17 00:34:08 -0400

    feat: added bootstrap and angular material

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,32 +1,44 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { io } from 'socket.io-client';
-import {FormsModule} from '@angular/forms';
+import {Component} from '@angular/core';
+import {RouterOutlet} from '@angular/router';
+import {io} from 'socket.io-client';
+import {FormControl, FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {CommonModule} from '@angular/common';
+import {MatInput} from '@angular/material/input';
+import {MatButtonModule} from '@angular/material/button';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, FormsModule, CommonModule],
+  imports: [RouterOutlet, MatInputModule, FormsModule, CommonModule, MatFormFieldModule, MatInput, ReactiveFormsModule, MatButtonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   private socket: any;
-  mensaje = '';
   mensajes: string[] = [];
+  messageInput: FormControl = new FormControl()
 
   ngOnInit() {
-    this.socket = io('http://localhost:3000'); // O tu IP/LAN si estás en otro dispositivo
+    this.socket = io('http://localhost:3000');
 
     this.socket.on('mensaje', (data: any) => {
       this.mensajes.push(data);
     });
   }
 
+  get message() {
+    return this.messageInput.value;
+  }
+
+  set message(value: string) {
+    this.messageInput.patchValue(value);
+  }
+
   enviar() {
-    if (this.mensaje.trim()) {
-      this.socket.emit('mensaje', this.mensaje);
-      this.mensaje = '';
+    if (this.message.trim()) {
+      this.socket.emit('mensaje', this.message);
+      this.message = '';
     }
   }
 
